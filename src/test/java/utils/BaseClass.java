92e747ebc3e604a707b0e5fefff654254dfc0dfc
Vishnupriya512 <kannanvishnupriya840@gmail.com>
2026-06-07 13:33:34 +0530

    Updated config.properties file

--- src/test/java/utils/BaseClass.java
+++ src/test/java/utils/BaseClass.java
@@ -2,6 +2,7 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BaseClass {
@@ -14,6 +15,8 @@ public class BaseClass {
         } else if (browser.equals("firefox")){
             driver = new FirefoxDriver();
 
+        } else if (browser.equals("edge")){
+            driver = new EdgeDriver();
         }
 
         driver.manage().window().maximize();
